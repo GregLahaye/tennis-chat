0b1b641558889944e501cda258208d5310c72015
Greg Lahaye <greg@lahayes.net>
2019-11-27 17:05:38 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+
+const animals = require('./animals.json');
+const colors = require('./colors.json');
+
+
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+  response.sendFile(`${__dirname}/views/index.html`);
+});
+
+io.on('connection', (socket) => {
+  socket.username = animals[Math.floor(Math.random() * animals.length)];
+  socket.color = colors[Math.floor(Math.random() * colors.length)];
+
+  io.emit('announcement', `${socket.username} joined the room`);
+
+  socket.on('message', (message) => {
+    io.emit('message', socket.username, socket.color, message);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('announcement', `${socket.username} left the room`);
+  });
+});
+
+http.listen(process.env.PORT || 8080);
